59c571f7f68a62592b2ed05541c85550a64025ec
Joel Conalty <joelconalty@gmail.com>
2023-08-10 16:43:19 +0100

    addBookToLibrary function prevents repeat titles in library

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Data structures
+
 let myLibrary = [];
 
 class Book {
@@ -9,22 +11,55 @@ class Book {
 	}
 }
 
-// function addBookToLibrary(newBook) {
-	
-// }
+// UI Elements
 
-let modal = document.getElementById('modal')
+let modal = document.getElementById("modal")
+let modalContent = document.getElementById("modal-content")
+let modalClose = document.getElementsByClassName("close")[0]
+let newBookButton = document.getElementById("book-btn")
+let submit = document.getElementById("submit-btn")
+let form = document.getElementById("modal-form")
 
-let btn = document.getElementById("book-btn")
-
-let span = document.getElementsByClassName('close')[0]
-
-btn.onclick = function() {
+newBookButton.onclick = function() {
 	modal.style.display = 'block'
+}
+
+modalClose.onclick = function(e) {
+	modal.style.display = "none"
 }
 
 window.onclick = function(e) {
 	if (e.target == modal) {
 		modal.style.display = "none"
 	}
+}
+
+// Use form data to create Book object
+const createNewBookObj = () => {
+	const title = document.getElementById('title').value
+	const author = document.getElementById('author').value
+	const pages = document.getElementById('pages').value
+	const read = document.getElementById('read').checked
+	return new Book(title, author, pages, read)
+}
+
+const addBookToLibrary = (e) => {
+	e.preventDefault()
+	let newBook = createNewBookObj();
+
+	for(let i = 0; i < myLibrary.length; i++) {
+		if(myLibrary[i].title === newBook.title) {
+			console.log("this book is already in your library")
+			console.log(myLibrary)
+			return;
+		}
+	}
+	myLibrary.push(newBook)
+	console.log(myLibrary)
+	return;
+}
+
+submit.onclick = function(e) {
+	addBookToLibrary(e);
+	form.reset()
 }
